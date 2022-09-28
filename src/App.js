ab8ea14ddcd8a73f91e463e4a1980fe4d8ac1876
Lukasz Kaminski <lukasznoroff@gmail.com>
2022-09-28 11:02:53 +0200

    add helmet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Forside from "./pages/Forside";
 import Tjenester from "./pages/Tjenester";
 import OmOss from "./pages/OmOss";
@@ -10,26 +10,34 @@ import Skanning from "./components/Skanning";
 import Landskap from "./components/Landskap";
 import Arkitektur from "./components/Arkitektur";
 import Bim from "./components/Bim";
+import { Helmet } from "react-helmet";
 
 
 function App() {
   return (
     <div className="App">
-        <GlobalStyle/>
-        <BrowserRouter>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Forside/>}/>
-                <Route path="Tjenester" element={<Tjenester/>}>
-                    <Route path="Skanning" element={<Skanning/>}/>
-                    <Route path="Landskap" element={<Landskap/>}/>
-                    <Route path="Arkitektur" element={<Arkitektur/>}/>
-                    <Route path="Bim" element={<Bim/>}/>
-                </Route>
-                <Route path="/OmOss" element={<OmOss/>}/>
-                <Route path="/Kontakt" element={<Kontakt/>}/>
-            </Routes>
-        </BrowserRouter>
+      <GlobalStyle />
+      <Helmet>
+        <title>Skan-lab</title>
+        <meta
+          name="description"
+          content="Skan-LAB konsulent firma tilbyr oppmåling, byggstiking, vegstikking, masseberegning, 3D skanning, BIM, visualisering, landskap og arkitektoniske prosjektering."
+        />
+      </Helmet>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Forside />} />
+          <Route path="Tjenester" element={<Tjenester />}>
+            <Route path="Skanning" element={<Skanning />} />
+            <Route path="Landskap" element={<Landskap />} />
+            <Route path="Arkitektur" element={<Arkitektur />} />
+            <Route path="Bim" element={<Bim />} />
+          </Route>
+          <Route path="/OmOss" element={<OmOss />} />
+          <Route path="/Kontakt" element={<Kontakt />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
